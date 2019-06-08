be963713ea6c34d29c638864030c14309d5871e6
Roopesh Kumar <imroopeshkumar@gmail.com>
2019-06-08 23:26:32 +1000

    completed pie charts added wordcloud

--- scriptv3.js
+++ scriptv3.js
@@ -151,7 +151,13 @@ let margin = { top: 50, right: 50, bottom: 50, left: 50 }
 
 //--------------------------------------------------------------nice pie chart month------------------------------------------------
 
+freqdata = null;
+daypiesvg = null;
+var div = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
 
+let selectedyear = null;
 
 
 var piewidth = 960,
@@ -179,29 +185,98 @@ var color = d3.scale.ordinal()
     .domain(["Lorem ipsum", "dolor sit", "amet", "consectetur", "adipisicing", "elit", "sed", "do", "eiusmod", "tempor", "incididunt"])
     .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
 
-d3.csv("http://118.138.62.59:9000/dataloc/frequencyofmonth.csv"
+d3.csv("http://118.138.62.59:9000/dataloc/freqdaymonthyear.csv"
     , function (data) {
-        console.log(data)
+        freqdata = data;
+        // console.log(data)
         var piesvg = d3.select("#drawing_area_pie_chart_month")
             .append("svg")
             // .attr('width', 1000).attr('height', 1000)
             .append("g")
 
 
+
         piedata2014 = []
         piedata2015 = []
         piedata2016 = []
+        piedata2017 = []
+        // piedata2017 = []
+
+
+
+
+
 
         data.forEach(element => {
             if (element.year == 2014) {
-                piedata2014.push({ 'label': element.month, 'value': element.n })
+                count =0
+
+
+                let found = piedata2014.some(item => item.label == element.month);
+                if(found){
+                    // console.log(found)
+                    piedata2014.forEach(e=> {
+                        if(e.label == element.month) {
+                            e.value += parseInt(element.n)
+                        }
+                    })
+                }
+
+                else {
+                    piedata2014.push({ 'label': element.month, 'value': parseInt(element.n) })
+                }
+                count+=1
+
             }
             else if (element.year == 2015) {
-                piedata2015.push({ 'label': element.month, 'value': element.n })
+
+
+                let found = piedata2015.some(item => item.label == element.month);
+                if(found){
+                    piedata2015.forEach(e=> {
+                        if(e.label == element.month) {
+                            e.value += parseInt(element.n)
+                        }
+                    })
+                }
+
+                else {
+                    piedata2015.push({ 'label': element.month, 'value': parseInt(element.n) })
+                }
+
             }
             else if (element.year == 2016) {
-                piedata2016.push({ 'label': element.month, 'value': element.n })
+
+                let found = piedata2016.some(item => item.label == element.month);
+                if(found){
+                    piedata2016.forEach(e=> {
+                        if(e.label == element.month) {
+                            e.value += parseInt(element.n)
+                        }
+                    })
+                }
+
+                else {
+                    piedata2016.push({ 'label': element.month, 'value': parseInt(element.n) })
+                }
             }
+            else if (element.year == 2017) {
+
+                let found = piedata2017.some(item => item.label == element.month);
+                if(found){
+                    piedata2016.forEach(e=> {
+                        if(e.label == element.month) {
+                            e.value += parseInt(element.n)
+                        }
+                    })
+                }
+
+                else {
+                    piedata2017.push({ 'label': element.month, 'value': parseInt(element.n) })
+                }
+            }
+
+
         });
 
         piesvg.append("g")
@@ -212,24 +287,43 @@ d3.csv("http://118.138.62.59:9000/dataloc/frequencyofmonth.csv"
             .attr("class", "lines");
 
 
-        piesvg.attr("transform", "translate(" + width / 4 + "," + height / 4 + ")");
+        piesvg.attr("transform", "translate(" + width / 3 + "," + height / 4 + ")");
 
         change(piesvg, piedata2014);
+        selectedyear = 2014;
+        
+        drawdayspie(selectedyear, 'Jan')
 
         d3.select("#piechartbutton2014")
             .on("click", function () {
                 change(piesvg, piedata2014);
+                selectedyear = 2014;
+                drawdayspie(selectedyear,'Jan')
+
             });
 
 
         d3.select("#piechartbutton2015")
             .on("click", function () {
                 change(piesvg, piedata2015);
+                drawdayspie(selectedyear,'Jan')
+                selectedyear = 2015
             });
 
         d3.select("#piechartbutton2016")
             .on("click", function () {
                 change(piesvg, piedata2016);
+                drawdayspie(selectedyear,'Jan')
+
+                selectedyear = 2016
+            });
+
+            d3.select("#piechartbutton2017")
+            .on("click", function () {
+                change(piesvg, piedata2017);
+                drawdayspie(selectedyear,'Jan')
+
+                selectedyear = 2017
             });
 
 
@@ -238,74 +332,79 @@ d3.csv("http://118.138.62.59:9000/dataloc/frequencyofmonth.csv"
             .innerRadius(0)
             .outerRadius(150 + 10);
 
-            piesvg.on('mousemove', (d) => {
-                d3.select(d.firstChild).transition()
-              .attr("d", arcOver);
-            })
     })
 
 
+function drawdayspie(year, month) {
+    selecteddata = 'piedata' + month;
 
-d3.csv("http://118.138.62.59:9000/dataloc/frequencyofday.csv", function (data) {
+    piedataJan = []
+    piedataFeb = []
 
-    var piesvg = d3.select("#drawing_area_pie_chart_month")
-        .append("svg")
-        // .attr('width', 1000).attr('height', 1000)
-        .append("g")
+    piedataMar = []
 
-    piedatajan = []
-    piedatafeb = []
+    piedataApr = []
+    piedataMay = []
+    piedataJun = []
+    piedataJul = []
+    piedataAug = []
+    piedataSep = []
+    piedataOct = []
+    piedataNov = []
+    piedataDec = []
+    freqdata.forEach(element => {
+        if (element.year == year) {
 
-    piedatamar = []
+            if (element.month == 'Jan') {
+                piedataJan.push({ 'label': element['Day_of_Week.x'], 'value': element.n })
+            }
+            else if (element.month == 'Feb') {
+                piedataFeb.push({ 'label': element['Day_of_Week.x'], 'value': element.n })
+            }
+            else if (element.month == 'Mar') {
+                piedataMar.push({ 'label': element['Day_of_Week.x'], 'value': element.n })
+            }
+            else if (element.month == 'Apr') {
+                piedataApr.push({ 'label': element['Day_of_Week.x'], 'value': element.n })
+            }
+            else if (element.month == 'May') {
+                piedataMay.push({ 'label': element['Day_of_Week.x'], 'value': element.n })
+            }
+            else if (element.month == 'Jun') {
+                piedataJun.push({ 'label': element['Day_of_Week.x'], 'value': element.n })
+            }
+            else if (element.month == 'Jul') {
+                piedataJul.push({ 'label': element['Day_of_Week.x'], 'value': element.n })
+            }
+            else if (element.month == 'Aug') {
+                piedataAug.push({ 'label': element['Day_of_Week.x'], 'value': element.n })
+            }
+            else if (element.month == 'Sep') {
+                piedataSep.push({ 'label': element['Day_of_Week.x'], 'value': element.n })
+            }
+            else if (element.month == 'Oct') {
+                piedataOct.push({ 'label': element['Day_of_Week.x'], 'value': element.n })
+            }
+            else if (element.month == 'Nov') {
+                piedataNov.push({ 'label': element['Day_of_Week.x'], 'value': element.n })
+            }
+            else if (element.month == 'Dec') {
+                piedataDec.push({ 'label': element['Day_of_Week.x'], 'value': element.n })
+            }
+        }
+    })
+    if(d3.select('#drawing_area_pie_chart_day').select('#daypiesvg').node()){
+        change(daypiesvg, this[selecteddata]);
+    }
+    else {
+    d3.select("#drawing_area_pie_chart_day").selectAll('svg').remove()
+        var piesvg = d3.select("#drawing_area_pie_chart_day")
+        .append("svg").attr('id', 'daypiesvg')
+        .attr('width', 960).attr('height', 540)
+        .append("g").attr('id', 'daypie')
 
-    piedataapr = []
-    piedatamay = []
-    piedatajun = []
-    piedatajul = []
-    piedataaug = []
-    piedatasep = []
-    piedataoct = []
-    piedatanov = []
-    piedatadec = []
 
-    data.forEach(element => {
-        if (element.month == 'Jan') {
-            piedatajan.push({ 'label': element['Day_of_Week.x'], 'value': element.n })
-        }
-        else if (element.month == 'Feb') {
-            piedatafeb.push({ 'label': element['Day_of_Week.x'], 'value': element.n })
-        }
-        else if (element.month == 'Mar') {
-            piedatamar.push({ 'label': element['Day_of_Week.x'], 'value': element.n })
-        }
-        else if (element.month == 'Apr') {
-            piedataapr.push({ 'label': element['Day_of_Week.x'], 'value': element.n })
-        }
-        else if (element.month == 'May') {
-            piedatamay.push({ 'label': element['Day_of_Week.x'], 'value': element.n })
-        }
-        else if (element.month == 'Jun') {
-            piedatajun.push({ 'label': element['Day_of_Week.x'], 'value': element.n })
-        }
-        else if (element.month == 'Jul') {
-            piedatajul.push({ 'label': element['Day_of_Week.x'], 'value': element.n })
-        }
-        else if (element.month == 'Aug') {
-            piedataaug.push({ 'label': element['Day_of_Week.x'], 'value': element.n })
-        }
-        else if (element.month == 'Sep') {
-            piedatasep.push({ 'label': element['Day_of_Week.x'], 'value': element.n })
-        }
-        else if (element.month == 'Oct') {
-            piedataoct.push({ 'label': element['Day_of_Week.x'], 'value': element.n })
-        }
-        else if (element.month == 'Nov') {
-            piedatanov.push({ 'label': element['Day_of_Week.x'], 'value': element.n })
-        }
-        else if (element.month == 'Dec') {
-            piedatadec.push({ 'label': element['Day_of_Week.x'], 'value': element.n })
-        }
-    });
+    
 
     piesvg.append("g")
         .attr("class", "slices");
@@ -315,24 +414,15 @@ d3.csv("http://118.138.62.59:9000/dataloc/frequencyofday.csv", function (data) {
         .attr("class", "lines");
 
 
-    piesvg.attr("transform", "translate(" + width / 4 + "," + height / 4 + ")");
+    piesvg.attr("transform", "translate(" + width / 3 + "," + height / 4 + ")");
+    // console.log(piedatajan)
+    change(piesvg, this[selecteddata]);
 
-    change(piesvg, piedatajan);
+    daypiesvg = piesvg
+    }
 
-    // d3.select("#jan")
-    //         .on("click", function () {
-    //             change(piesvg,piedatajan);
-    //         });
-    d3.select("#Feb")
-        .on("click", function () {
-            change(piesvg, piedatafeb);
-        });
+}
 
-    
-
-
-
-});
 
 
 
@@ -344,14 +434,18 @@ function change(piesvg, data) {
     var slice = piesvg.select(".slices").selectAll("path.slice")
         .data(pie(data), key);
 
-    console.log
+    // console.log
 
     slice.enter()
         .insert("path")
         .style("fill", function (d) {
             return color(d.data.label);
         })
-        .attr("class", "slice");
+        // .style('opacity', 0.4)
+        .attr("class", "slice")
+        .attr('id', (d) => {
+            return d.value
+        });
 
     slice
         .transition().duration(1000)
@@ -431,4 +525,103 @@ function change(piesvg, data) {
 
     polyline.exit()
         .remove();
+
+    
+    let current = null;
+
+    slice.on("mouseover", function (d) {
+        div.transition()
+            .duration(200)
+            .style("opacity", .9);
+
+        div.html(d.data.value)
+            .style("left", (d3.event.pageX) + "px")
+            .style("top", (d3.event.pageY - 28) + "px");
+
+
+
+            current = this;  
+            var others = piesvg.selectAll(".slice").filter(function(el) {
+              return this != current
+            });
+
+            d3.select(this).style('opacity', 0.3);
+    })
+        .on("mouseout", function (d) {
+            div.transition()
+                .duration(500)
+                .style("opacity", 0);
+                d3.select(current).style('opacity', 1);
+        })
+        .on('click', function(d) {
+            if(d.data.label.substring(d.data.label.length-3, d.data.label.length)== 'day') {
+                console.log('do nothing')
+            } else {
+                drawdayspie(selectedyear, d.data.label)
+            }
+        })
 };
+
+d3.csv("http://118.138.62.59:9000/dataloc/frequencyofmake.csv"
+    , function (data) {
+        console.log(data)
+
+        var word_count = {};
+        data.forEach(function(word){
+            // var word = word.toLowerCase();
+            // if (word != "" &&  word.length>1){
+            //   if (word_count[word]){
+            //     word_count[word]++;
+            //   } else {
+            //     word_count[word] = 1;
+            //   }
+            // }
+
+            word_count[word.make] = word.n
+          })
+
+          var svg_location = "#drawing_area_word_cloud";
+          width = 540
+          height = 960
+          var fill = d3.scale.category20();
+          var word_entries = d3.entries(word_count);
+
+          var xScale = d3.scale.linear()
+           .domain([0, d3.max(word_entries, function(d) {
+              return d.value;
+            })
+           ])
+           .range([10,100]);
+
+           d3.layout.cloud().size([width, height])
+          .timeInterval(20)
+          .words(word_entries)
+          .fontSize(function(d) { return xScale(+d.value); })
+          .text(function(d) { return d.key; })
+          .rotate(function() { return ~~(Math.random() * 2) *90; })
+          .font("Impact")
+          .on("end", draw)
+          .start();
+
+          function draw(words) {
+            d3.select(svg_location).append("svg")
+                .attr("width", width)
+                .attr("height", height)
+              .append("g").attr('id', 'wordcloudsvg')
+                .attr("transform", "translate(" + [width >> 1, height >> 1] + ")")
+              .selectAll("text")
+                .data(words)
+              .enter().append("text")
+                .style("font-size", function(d) { return xScale(d.value) + "px"; })
+                .style("font-family", "Impact")
+                .style("fill", function(d, i) { return fill(i); })
+                .attr("text-anchor", "middle")
+                .attr("transform", function(d) {
+                  return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+                })
+                .text(function(d) { return d.key; });
+          }
+          d3.layout.cloud().stop();
+
+
+    });
